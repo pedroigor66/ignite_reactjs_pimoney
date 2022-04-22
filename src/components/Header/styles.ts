3e5ae0dafe0c9ba524ba281dce24f7d1bd929d9b
Pedro Igor <picoliveira92@gmail.com>
2022-04-21 21:04:36 -0300

    Part 2.1 - Header component created and styled

---
+++ src/components/Header/styles.ts
@@ -0,0 +1,34 @@
+import styled from "styled-components";
+
+export const Container = styled.header`
+  background: var(--blue);
+`;
+
+export const Content = styled.div`
+  max-width: 1120px;
+  // to keep it centered in the page
+  margin: 0 auto;
+
+  // 0 16px (so the icon does not stick to the edge when screen size shrinks) and 160px
+  padding: 2rem 1rem 12rem;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  button {
+    font-size: 1rem;
+    color: #fff;
+    background: var(--blue-light);
+    border: 0;
+    padding: 0 2rem;
+    border-radius: 0.25rem;
+    height: 3rem;
+
+    // transition and hover are for cool effects when hovering the button
+    transition: filter 0.2s;
+
+    &:hover {
+      filter: brightness(0.9);
+    }
+  }
+`;
